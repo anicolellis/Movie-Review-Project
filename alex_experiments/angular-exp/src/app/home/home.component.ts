a0c472346bc65ef819ffff2456899e75a64e4ada
anicolellis <alexnicolellis1@gmail.com>
2025-01-28 18:04:38 -0600

    movie display

--- alex_experiments/angular-exp/src/app/home/home.component.ts
+++ alex_experiments/angular-exp/src/app/home/home.component.ts
@@ -1,35 +1,37 @@
 import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {HousingLocationComponent} from '../housing-location/housing-location.component';
-import {HousingLocation} from '../housinglocation';
-import {HousingService} from '../housing.service';
+//import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import {Movie} from '../movie';
+import { MovieComponent } from '../movie/movie.component';
+//import {HousingService} from '../housing.service';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, HousingLocationComponent],
+  imports: [CommonModule, MovieComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 
 export class HomeComponent {
-  housingLocationList: HousingLocation[] = [];
-  housingService: HousingService = inject(HousingService);
-  filteredLocationList: HousingLocation[] = [];
+  movieList: Movie[] = [];
+  movieService: MovieService = inject(MovieService);
+  filteredMovieList: Movie[] = [];
 
   constructor() {
-    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
-      this.housingLocationList = housingLocationList;
-      this.filteredLocationList = housingLocationList;
+    this.movieService.getAllMovies().then((movieList: Movie[]) => {
+      this.movieList = movieList;
+      this.filteredMovieList = movieList;
     });
   }
   filterResults(text: string) {
     if(!text) {
-      this.filteredLocationList = this.housingLocationList;
+      this.filteredMovieList = this.movieList;
       return;
     }
 
-    this.filteredLocationList = this.housingLocationList.filter((housingLocation) => 
-      housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    this.filteredMovieList = this.movieList.filter((movie) => 
+      movie?.title.toLowerCase().includes(text.toLowerCase())
     );
   }
 }
