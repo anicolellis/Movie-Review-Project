c1a381f5cfd166ad09f4bfbf1df6e57065eb1c99
anicolellis <alexnicolellis1@gmail.com>
2025-01-28 18:58:15 -0600

    create form

---
+++ alex_experiments/angular-exp/src/app/movie-form/movie-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, inject } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms'
+import { CommonModule } from '@angular/common';
+import { MovieService } from '../movie.service';
+
+@Component({
+  selector: 'app-movie-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './movie-form.component.html',
+  styleUrl: './movie-form.component.css'
+})
+export class MovieFormComponent {
+  movieService = inject(MovieService);
+  mForm = new FormGroup({
+    title: new FormControl(''),
+    director: new FormControl('')
+  });
+
+  submitMovie() {
+    this.movieService.submitMovie(
+      this.mForm.value.title ?? '',
+      this.mForm.value.director ?? '',
+    );
+  }
+}
