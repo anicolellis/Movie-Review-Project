08d01122fd975d7b8541966524f71076b6fbe16f
anicolellis <alexnicolellis1@gmail.com>
2025-02-26 17:59:01 -0600

    basic version of movie feed

--- Frontend/MovieApp/src/app/movie-feed/movie-feed.component.ts
+++ Frontend/MovieApp/src/app/movie-feed/movie-feed.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movie-feed',
@@ -7,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './movie-feed.component.css'
 })
 export class MovieFeedComponent {
-
+  movieList: Movie[] = [];
+  movieService: MovieService = inject(MovieService);
+  
+  constructor() {
+    this.movieService.getAllMovies() // TODO
+  }
 }
