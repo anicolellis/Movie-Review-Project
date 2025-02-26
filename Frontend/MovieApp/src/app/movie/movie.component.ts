c18cb7fdb617232fc057614bb5b61b611b4c8668
anicolellis <alexnicolellis1@gmail.com>
2025-02-26 17:48:58 -0600

    movie component skeleton

--- Frontend/MovieApp/src/app/movie/movie.component.ts
+++ Frontend/MovieApp/src/app/movie/movie.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from '../movie';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movie',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './movie.component.html',
   styleUrl: './movie.component.css'
 })
 export class MovieComponent {
-
+  @Input() movie!: Movie;
 }
